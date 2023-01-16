fe8d76f040b11f2ac2592c9a880850a888bd9bb4
Rawle Juglal <rawledjuglal@gmail.com>
2023-01-15 20:50:11 -0600

    add feature
    confetti js for when the record is broken

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
+let confettiSettings = { target: 'my-canvas', animate:true };
+let confetti = new ConfettiGenerator(confettiSettings);
+
 let saveEl = document.getElementById("save-el")
 let countEl = document.getElementById("count-el")
 let count = 0
+let prevRecord = {
+    entries:0,
+    current:0
+}
 
 function increment() {
     count += 1
@@ -10,6 +17,24 @@ function increment() {
 function save() {
     let countStr = count + " - "
     saveEl.textContent += countStr
+    isRecordBreaker()
     countEl.textContent = 0
-    count = 0
+    count = 0  
 }
+
+function isRecordBreaker(){
+    if(prevRecord.entries > 0 && count > prevRecord.current){
+        let confettiSettings = { target: 'my-canvas', animate:true };
+        let confetti = new ConfettiGenerator(confettiSettings);
+        confetti.render();
+        prevRecord.entries += prevRecord.entries+1;
+        setTimeout(()=>{
+            confetti.clear()
+            document.getElementById('my-canvas').removeAttribute('height')
+            document.getElementById('my-canvas').removeAttribute('width')
+        }, 3000)
+    } else {
+        prevRecord.entries += prevRecord.entries+1;
+    }
+}
+
